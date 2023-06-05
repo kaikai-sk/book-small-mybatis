52be7fead8c1c95db67dab55642dc73ad4f55e01
kaikai-sk <1431173103@qq.com>
2023-06-05 22:20:12 +0800

    手写第二章

--- mybatis-step-02-sk/src/main/java/cn/bugstack/mybatis/binding/MapperProxyFactory.java
+++ mybatis-step-02-sk/src/main/java/cn/bugstack/mybatis/binding/MapperProxyFactory.java
@@ -3,14 +3,7 @@ package cn.bugstack.mybatis.binding;
 import java.lang.reflect.Proxy;
 import java.util.Map;
 
-/**
- * @author 小傅哥，微信：fustack
- * @description 映射器代理工厂
- * @github https://github.com/fuzhengwei
- * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
 public class MapperProxyFactory<T> {
-
     private final Class<T> mapperInterface;
 
     public MapperProxyFactory(Class<T> mapperInterface) {
@@ -21,5 +14,4 @@ public class MapperProxyFactory<T> {
         final MapperProxy<T> mapperProxy = new MapperProxy<T>(sqlSession, mapperInterface);
         return (T) Proxy.newProxyInstance(mapperInterface.getClassLoader(), new Class[]{mapperInterface}, mapperProxy);
     }
-
 }
