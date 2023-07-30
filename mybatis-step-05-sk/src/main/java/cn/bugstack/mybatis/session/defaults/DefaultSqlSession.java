f40a84001b9b589a80802c891494f4822dbdb34e
kaikai-sk <1431173103@qq.com>
2023-07-30 20:28:01 +0800

    第四章

---
+++ mybatis-step-05-sk/src/main/java/cn/bugstack/mybatis/session/defaults/DefaultSqlSession.java
@@ -0,0 +1,36 @@
+package cn.bugstack.mybatis.session.defaults;
+
+import cn.bugstack.mybatis.binding.MapperRegistry;
+import cn.bugstack.mybatis.mapping.MappedStatement;
+import cn.bugstack.mybatis.session.Configuration;
+import cn.bugstack.mybatis.session.SqlSession;
+
+public class DefaultSqlSession implements SqlSession {
+
+    private Configuration configuration;
+
+    public DefaultSqlSession(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public <T> T selectOne(String statement) {
+        return (T) ("你的操作被代理了！" + statement);
+    }
+
+    @Override
+    public <T> T selectOne(String statement, Object parameter) {
+        MappedStatement mappedStatement = configuration.getMappedStatement(statement);
+        return (T) ("你的操作被代理了！" + "\n方法：" + statement + "\n入参：" + parameter + "\n待执行SQL：" + mappedStatement.getSql());
+    }
+
+    @Override
+    public <T> T getMapper(Class<T> type) {
+        return configuration.getMapper(type, this);
+    }
+
+    @Override
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+}
